cb4625f22e1d29aa16284745861c65458cc62fa0
Bekir Berke <76753313+Bekir-Berke@users.noreply.github.com>
2023-11-12 13:49:27 +0300

    Initial Commit

---
+++ eCommerce/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eCommerce
+//
+//  Created by Bekir Berke Yılmaz on 12.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eCommerce/eCommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  eCommerceApp.swift
+//  eCommerce
+//
+//  Created by Bekir Berke Yılmaz on 12.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct eCommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
